8cccecd6e28f7943bf99a3a59267daa17c46414c
Daniel da Silva Pedro <daniel.pedro@labsoft.com.br>
2023-02-23 14:40:52 -0300

    What is browser context and Page fixtures in Playwright

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -3,10 +3,28 @@ const {test} = require('@playwright/test');
 
 // ASYNC is required to tell javascript not to execute code asynchronously, without them, AWAIT not work
 // Anonymous function created async ()=> {}
-test('First Playwright Test', async ()=> 
+// browser comes from annotation test, no need to declare it, it's global fixtures
+test('Browser Context Create Playwright Test', async ({ browser })=> 
 {
     // PlayWright code here
-    
+
+    // Default
+    // Browser - Create New Context without cookies or plugins
+    const context = await browser.newContext();
+    // Create a new page
+    const page = await context.newPage();
 
 
+    // Navigate to one page
+    await page.goto("https://sso.teachable.com/secure/9521/identity/login/password");
+});
+
+
+
+// page it's global and will use default browser instances
+test('Page Playwright Test', async ({ page })=> 
+{
+    // PlayWright code here
+
+    await page.goto("https://sso.teachable.com/secure/9521/identity/login/password");
 });
