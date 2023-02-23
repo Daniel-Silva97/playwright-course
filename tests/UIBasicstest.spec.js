bb565aab39d0d5d237d62812965410c772db4bd0
Daniel da Silva Pedro <daniel.pedro@labsoft.com.br>
2023-02-23 14:16:15 -0300

    First Commit

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,12 @@
+// Import annotation with all playwright jobs
+const {test} = require('@playwright/test');
+
+// ASYNC is required to tell javascript not to execute code asynchronously, without them, AWAIT not work
+// Anonymous function created async ()=> {}
+test('First Playwright Test', async ()=> 
+{
+    // PlayWright code here
+    
+
+
+});
