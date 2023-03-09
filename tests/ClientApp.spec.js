b665d90d8b0ea4f089415dce53195ffa78cffb91
Daniel Silva <danielsp699@outlook.com>
2023-03-09 16:09:03 -0300

    #Comment
    - Demo of the Application and Test Scenarios which needs to be automate
    -Write the Script to dynamically
    find the product to buy from list of products
    - Add assertions for the actions performed
    and implement necessary Sync steps

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -3,15 +3,16 @@ const {test, expect} = require('@playwright/test');
 
 
 // page it's global and will use default browser instances
-test('Browser Context-Validating Error Login', async ({ page })=> {
-
-    const userName = page.locator("#userEmail");
-    const password = page.locator("#userPassword");
-
+test.only('Browser Context-Validating Error Login', async ({ page })=> {
 
 
     await page.goto("https://rahulshettyacademy.com/client");
-    
+
+    const userName = page.locator("#userEmail");
+    const password = page.locator("#userPassword");
+    const products = page.locator(".card-body");
+    const productName = 'adidas original';
+   
     await userName.fill("teste@playwright.com");
     await password.fill("Playwright@123");
 
@@ -22,6 +23,31 @@ test('Browser Context-Validating Error Login', async ({ page })=> {
 
     // allTextContents do not wait until the page is loaded
     const allTitles = await page.locator(".card-body b").allTextContents();
-    console.log(allTitles);
+    console.log(allTitles)
 
+
+    //Getting total products
+    const count = await products.count();
+
+    for(let i = 0; i < count; i++) {
+        // Conditional check if the text present in tag 'b' equals productName
+        if (await products.nth(i).locator("b").textContent() === productName) {
+            // Add to cart
+            await products.nth(i).locator("text= Add To Cart").click();
+            break;
+        }
+            
+    }
+
+    // click in cart
+    await page.locator("[routerlink*='cart']").click();
+
+    // waiting until the first li list load on the page before do the assertion
+    await page.locator("div li").first().waitFor();
+
+    //check if some tag h3 has the text adidas original
+    const bool = await page.locator("h3:has-text('adidas original')").isVisible();
+    //Assertion to see that's ok
+    expect(bool).toBeTruthy();
+    
 });
