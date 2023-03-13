d5e7dc44b4b20ca19ad974bc2e2498cfc60fce34
Daniel Silva <danielsp699@outlook.com>
2023-03-13 15:11:06 -0300

    #Comment
    - End to end validation with mix of API
    & Web concepts - Reduce test time

--- tests/WebApiPart1.spec.js
+++ tests/WebApiPart1.spec.js
@@ -1,8 +1,9 @@
 const {test, expect, request} = require("@playwright/test")
 
 const loginPayload = {userEmail:"teste@playwright.com",userPassword:"Playwright@123"};
-
+const orderPayload = {orders:[{country:"Brazil",productOrderedId:"6262e990e26b7e1a10e89bfa"}]};
 let token;
+let orderID;
 
 // Execute one time before all tests cases
 test.beforeAll( async() => {
@@ -26,14 +27,31 @@ test.beforeAll( async() => {
 
     // Extract token from JSON
     token = loginResponseJSON.token;
-
     console.log(token);
+
+
+    // 
+    const orderResponse = await apiContext.post(
+        "https://rahulshettyacademy.com/api/ecom/order/create-order",
+        {
+            data: orderPayload,
+            headers: {
+                'Authorization': token,
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+    
+    const orderResponseJSON = await orderResponse.json();
+    console.log(orderResponseJSON);
+    orderID = orderResponseJSON.orders[0];
+    console.log(orderID);
 });
 
 // Execute before each test, with 3 tests, there will be 3 executions.
 // test.beforeEach(() => {});
 
-test('Place the order', async ({ page })=> {
+test.only('Place the order', async ({ page })=> {
 
     // Add to Aplication -> Local Storage the token extract from API request
     // JavaScript initial script
@@ -41,89 +59,21 @@ test('Place the order', async ({ page })=> {
         window.localStorage.setItem('token', value);
     }, token);
 
-
-    const products = page.locator(".card-body");
-    const productName = 'adidas original';
-    const email = "teste@playwright.com";
-
     await page.goto("https://rahulshettyacademy.com/client");
 
-    // allTextContents do not wait until the page is loaded
-    const allTitles = await page.locator(".card-body b").allTextContents();
-    console.log(allTitles);
-
-
-    //Getting total products
-    const count = await products.count();
-
-    for(let i = 0; i < count; i++) {
-        // Conditional check if the text present in tag 'b' equals productName
-        if (await products.nth(i).locator("b").textContent() === productName) {
-            // Add to cart
-            await products.nth(i).locator("text= Add To Cart").click();
-            break;
-        }
-            
-    }
-
-    // click in cart
-    await page.locator("[routerlink*='cart']").click();
-
-    // waiting until the first li list load on the page before do the assertion
-    await page.locator("div li").first().waitFor();
-
-    //check if some tag h3 has the text adidas original
-    const bool = await page.locator("h3:has-text('adidas original')").isVisible();
-    //Assertion to see that's ok
-    expect(bool).toBeTruthy();
-
-
-    //Click checkout
-    await page.locator("text=Checkout").click();
-
-    // Typing in dropdown country, delay is to slow type in the page to load the suggestions in dropdown
-    await page.locator("[placeholder*='Country']").type("ind", {delay:100});
-
-    // wait for suggestions in dropdown show up
-    const dropdown = page.locator(".ta-results");
-    await dropdown.waitFor();
-
-    // counting...
-    const options = await dropdown.locator("button").count();
-
-    for(let i = 0; i < options; i++) {
-        const text = await dropdown.locator("button").nth(i).textContent();
-        if (text.trim() === "India") {
-            // click operation in right option
-            await dropdown.locator("button").nth(i).click();
-            break;
-        }
-    }
-
-    // chech if email typed here equal our login email
-    await expect(page.locator(".user__name label[type='text']")).toHaveText(email);
-
-    // Click in place order
-    await page.locator(".action__submit").click();
-
-    // Check if the page contain this text
-    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
-
-    // Allocating order ID without space and '|'
-    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
-
-
     // Go to My Orders
-    await page.locator("label[routerlink*='myorders']").click();
+    await page.locator("button[routerlink*='myorders']").click();
     
     const rows = page.locator("tbody tr");
 
     await page.locator("tbody").waitFor();
 
+    
+
     for( let i = 0; i < await rows.count(); i++) {
         const rowOrderId = await rows.nth(i).locator("th").textContent();
         // If the order exists here
-        if (orderId.includes(rowOrderId)) {
+        if (orderID.includes(rowOrderId)) {
             // Click in View Button
             await rows.nth(i).locator("button").first().click();
             break;
@@ -131,5 +81,5 @@ test('Place the order', async ({ page })=> {
     } 
     
     const orderDetailsId = await page.locator(".col-text").textContent();
-    expect(orderId.includes(orderDetailsId)).toBeTruthy();
+    expect(orderID.includes(orderDetailsId)).toBeTruthy();
 });
