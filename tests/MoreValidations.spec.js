d78cc9c48cd47a81eb7757406488c45eeb6a2b83
Daniel Silva <danielsp699@outlook.com>
2023-03-13 11:41:06 -0300

    #Comment
    - How to automate Java/JavaScript Alert popups with Playwright

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -10,11 +10,37 @@ test("Popup validations", async({page}) => {
     // Go to next page (->)
     // await page.goForward();
 
+    // Check if element is visible on page
     await expect(page.locator("#displayed-text")).toBeVisible();
-
+    // Click button to hide element
     await page.locator("#hide-textbox").click();
-
+    // Check if element is hidden
     await expect(page.locator("#displayed-text")).toBeHidden();
 
+    
+    // Handling with Java pop-ups
 
+    // Waiting on event called dialog, thats the first argument, the second argument, 
+    // we'll tell that if playwright clicks on Ok or Cancel button
+    // Parameter dialog.accept() to OK, and dialog.dismiss() to cancel
+
+    /*
+    page.on(event, action)
+
+    event -> dialog
+    action -> Accept or Cancel
+    */
+
+    await page.pause()
+    // OK, he will wait until the dialog appears, even if the action doesn't happen yet.
+    page.on('dialog', dialog => dialog.accept());
+    // Click after to see playwright waiting
+    await page.locator("#confirmbtn").click();
+
+    // Cancel
+    // page.on('dialog', dialog => dialog.dismiss());
+
+
+    // Mouse hover options
+    await page.locator("#mousehover").hover();
 });
