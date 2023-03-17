7538a0a37b9e49c8c544c29af8f663715bdb8c69
Daniel Silva <danielsp699@outlook.com>
2023-03-17 10:50:49 -0300

    #Comment
    - Implementing Parameterization in running
    tests with different data sets

--- tests/ClientAppPageObject.spec.js
+++ tests/ClientAppPageObject.spec.js
@@ -4,41 +4,42 @@ const { POManager } = require("../pages/POManger");
 // JSON -> String (stringfy) -> JS OBject (parse)
 const dataSet = JSON.parse(JSON.stringify(require("../data/ClientAppPageObjectTestData.json")));
 
+for(const data of dataSet) {
+    test(`Data Array Test ${data.productName}`, async ({ page })=> {
+        const poManager = new POManager(page);
 
-test('Browser Context-Validating Error Login', async ({ page })=> {
-    const poManager = new POManager(page);
-
-    // Login
-    const loginPage = poManager.getLoginPage();
-    await loginPage.goTo();
-    await loginPage.validLogin(dataSet.username, dataSet.password);
-   
-
-    // Dashboard
-    const dashboardPage = poManager.getDashboardPage();
-    await dashboardPage.searchProductAddCart(dataSet.productName);
-    await dashboardPage.navigateToCart();
-
-    // Cart
-    const cartPage = poManager.getCartPage();
-    await cartPage.verifyProductIsDisplayed(dataSet.productName);
-    await cartPage.checkOut();
-
-    // Checkout
-    const checkoutPage = poManager.getCheckoutPage();
-    await checkoutPage.verifyCountryAndSelect(dataSet.countryCode, dataSet.countryName);
-    await checkoutPage.verifyEmail(dataSet.username);
-    await checkoutPage.submitOrder();
-
-    // Order Confirmation
-    const orderConfirmationPage = poManager.getOrderConfirmatioPage();
-    await orderConfirmationPage.checkOrderReview(dataSet.thanksText);
-    const orderId = await orderConfirmationPage.getOrderId();
-    console.log(orderId);
-    await orderConfirmationPage.goToMyorders();
+        // Login
+        const loginPage = poManager.getLoginPage();
+        await loginPage.goTo();
+        await loginPage.validLogin(data.username, data.password);
     
-    // My Orders
-    const myOrdersPage = poManager.getMyOrdersPage()
-    await myOrdersPage.selectOrder(orderId);
-    expect(orderId.includes(await myOrdersPage.getOrderId())).toBeTruthy();
-});
+
+        // Dashboard
+        const dashboardPage = poManager.getDashboardPage();
+        await dashboardPage.searchProductAddCart(data.productName);
+        await dashboardPage.navigateToCart();
+
+        // Cart
+        const cartPage = poManager.getCartPage();
+        await cartPage.verifyProductIsDisplayed(data.productName);
+        await cartPage.checkOut();
+
+        // Checkout
+        const checkoutPage = poManager.getCheckoutPage();
+        await checkoutPage.verifyCountryAndSelect(data.countryCode, data.countryName);
+        await checkoutPage.verifyEmail(data.username);
+        await checkoutPage.submitOrder();
+
+        // Order Confirmation
+        const orderConfirmationPage = poManager.getOrderConfirmatioPage();
+        await orderConfirmationPage.checkOrderReview(data.thanksText);
+        const orderId = await orderConfirmationPage.getOrderId();
+        console.log(orderId);
+        await orderConfirmationPage.goToMyorders();
+        
+        // My Orders
+        const myOrdersPage = poManager.getMyOrdersPage()
+        await myOrdersPage.selectOrder(orderId);
+        expect(orderId.includes(await myOrdersPage.getOrderId())).toBeTruthy();
+    });
+}
